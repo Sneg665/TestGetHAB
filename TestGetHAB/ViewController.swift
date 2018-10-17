cab34c527a04ae5eee035b873627200d01ce96e8
Владимир Уланов <vovasneg@mail.ru>
2018-10-17 12:56:18 +0300

    //добавил кнопку

--- TestGetHAB/ViewController.swift
+++ TestGetHAB/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var ButtonTest: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let test1 = "privet"
+        //var kol = test+1
+        ButtonTest.setTitle(test1, for: .normal)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
