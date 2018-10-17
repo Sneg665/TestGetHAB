285bf76e11973b68fff922f85ba020e96268e6bc
Владимир Уланов <vovasneg@mail.ru>
2018-10-17 12:33:29 +0300

    Initial Commit

---
+++ TestGetHAB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestGetHAB
+//
+//  Created by Владимир Уланов on 17.10.2018.
+//  Copyright © 2018 UlanovApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
