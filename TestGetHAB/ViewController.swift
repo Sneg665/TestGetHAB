c0619df82a5e6fa1b5d7357e81827cfd507ac7ba
Владимир Уланов <vovasneg@mail.ru>
2018-10-22 17:37:48 +0300

    //dobavil cveta

--- TestGetHAB/ViewController.swift
+++ TestGetHAB/ViewController.swift
@@ -9,7 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var fonarikON = false
+    
+    
+    @IBAction func reCvet(_ sender: UIButton) {
+        textLabelLove.text = "Давай фонарик"
+        fonarikON = !fonarikON //это мы даём другое значение
+        pomeniaemCvet()
+        
+    }
+    func pomeniaemCvet(){
+        view.backgroundColor = fonarikON ? .blue: .red
+        
+    }
     @IBOutlet weak var textLabelLove: UILabel!
     @IBAction func sayLove(_ sender: UIButton) {
         textLabelLove.text = "Я люблю тебя Инесса!"
@@ -17,7 +29,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var ButtonTest: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        let test1 = "Инесса нажми на меня!"
+        let test1 = "Инесса, нажми на меня!"
         //var kol = test+1
         ButtonTest.setTitle(test1, for: .normal)
         // Do any additional setup after loading the view, typically from a nib.
